b4996e259d5c7a0aea46605ca1c6055c11239b59
Mirrorbreak <m1rr0rbr34k@gmail.com>
2017-10-24 15:00:23 +0200

    [2nd Battalion] Altis Experimental Update
    -Added Add Parachute button
    -Changed helmets to Scorpion Lite
    -Fixed Recon Leader having Desert camo, he has woodland like everyone else now
    -Increased Basic Rifleman ammo count by 1 Mag
    -Removed Weapon teams UI from spec ops
    -Added Sniper/Spotter (Ghillie) to basic menu

---
+++ 2nd Battalion SUT/Altis/TG_2ndBatt_ExperimentalOpsV0,2.Altis/configs/functions.hpp
@@ -0,0 +1,93 @@
+class TGFunctions
+	{
+		tag = "TG";
+		class general
+		{
+			file = "functions";
+
+		};
+		class StandardKits
+		{
+			file = "functions";
+
+			class AntiAir {};
+			class AntiAirAmmoBearer{};
+			class AR {};
+			class ARAmmoBearer {};
+			class CLS {};
+			class Engineer {};
+			class EngineerAmmoBearer {};
+			class Grenadier {};
+			class GrenadierAmmoBearer {};
+			class JTAC {};
+			class Leader {};
+			class LightAT {};
+			class LRRM {};
+			class Marksman {};
+			class MedAT {};
+			class MedATAmmoBearer {};
+			class MG {};
+			class Mortar_tube {};
+			class Mortar_bipod {};
+			class Rifleman {};
+			class Sniper {};
+			class Spotter {};
+		};
+		class ReconKits
+		{
+			file = "functions\ReconKits";
+
+			class ARRecon {};
+			class CLSRecon {};
+			class LeaderRecon {};
+			class ScoutRecon {};
+		};
+		class DivingKits
+		{
+			file = "functions\DivingKits";
+
+			class CLSDiving {};
+			class EngineerDiving {};
+			class LeaderDiving {};
+			class RiflemanDiving {};
+		};
+		class SpecOpsKits
+		{
+			file = "functions\SpecOpsKits";
+
+			class ARSpecOps {};
+			class AssaultSpecOps {};
+			class BreacherSpecOps {};
+			class CLSSpecOps {};
+			class HeavySniperSpecOps {};
+			class JTACSpecOps {};
+			class LeaderSpecOps {};
+			class LightATSpecOps {};
+			class MarksmanSpecOps {};
+			class PointmanSpecOps {};
+			class SignalerSpecOps {};
+			class SniperSpecOps {};
+			class SpotterSpecOps {};
+		};
+
+		class CommonKits
+		{
+			file = "functions\CommonKits";
+
+			class HeliPilot {};
+			class JetPilot {};
+			class Crewman {};
+			class HeliCrewman {};
+			class PlaneCrewman {};
+			class Parachute {};
+		};
+
+		class CommonFunctions
+		{
+			file = "functions\CommonFunctions";
+
+			class Countdown {};
+			class StaticParadrop {};
+			class StartStaticParadrop {};
+		}
+	};
---
+++ 2nd Battalion SUT/Altis/TG_2ndBatt_ExperimentalOpsV0,2.Altis/functions/CommonKits/fn_Parachute.sqf
@@ -0,0 +1,5 @@
+removeBackpack player;
+removeGoggles player;
+
+player addBackpack "rhs_d6_Parachute_backpack";
+player addGoggles "G_Balaclava_TI_G_blk_F";
